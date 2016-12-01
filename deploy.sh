8c0dda91706c9b8934da320bde072a3f455087ce
Michael J Hernandez <michael.j.hernandez@intel.com>
2016-12-01 13:56:24 -0800

    Add deploy debug messages

--- deploy.sh
+++ deploy.sh
@@ -67,4 +67,6 @@ rsync -rt --delete --exclude=".git" --exclude=".nojekyll" --exclude=".travis.yml
 cd $TARGET_DIR
 git add -A .
 git commit --allow-empty -m "Built from commit $REV"
+echo $REPO
+echo $TARGET_BRANCH
 git push $REPO $TARGET_BRANCH
